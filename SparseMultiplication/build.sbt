4b904aece432c966618ac371f937550d54ee5f9f
akhil <akhiljm@gmail.com>
2019-08-06 12:55:32 -0700

    First commit

---
+++ SparseMultiplication/build.sbt
@@ -0,0 +1,10 @@
+name := "SparseMultiplication"
+
+version := "0.1"
+
+scalaVersion := "2.11.9"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.2.0"
---
+++ SparseMultiplication/src/main/scala/sparsemultiplication/Main.scala
@@ -0,0 +1,73 @@
+package sparsemultiplication
+
+object Main {
+  import org.apache.spark.{SparkConf, SparkContext}
+  import org.apache.spark.mllib.linalg.distributed.{CoordinateMatrix, MatrixEntry}
+  import org.apache.spark.Partitioner
+
+
+  class LeftMatrixPartitioner(override val numPartitions: Int) extends Partitioner {
+    override def getPartition(key: Any): Int = {
+      print(s"Key$key")
+      key match {
+        case  MatrixEntry(i, j, v) => j.hashCode()% numPartitions
+        case _ =>  key.hashCode()% numPartitions
+      }
+    }
+  }
+
+  class RightMatrixPartitioner(override val numPartitions: Int) extends Partitioner {
+    override def getPartition(key: Any): Int =
+      key match {
+        case MatrixEntry => key.asInstanceOf[MatrixEntry].i.hashCode()
+        case _ => 0
+      }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("First App")
+
+    val sc = new SparkContext(conf)
+
+    val mat: CoordinateMatrix = getMatrix("Output.txt", sc)
+    val mat2: CoordinateMatrix = getMatrix("Output2.txt", sc)
+
+    val result: CoordinateMatrix = coordinateMatrixMultiply(mat, mat2)
+    result.entries.collect()
+  }
+
+  def getMatrix(fileName:String, sc: SparkContext) : CoordinateMatrix = {
+
+    val f = sc.textFile(fileName)
+
+    val data = f.map(_.split(',') match {
+      case Array(row, col, value) =>
+        MatrixEntry(row.toInt, col.toInt, value.toDouble)
+    }
+    )
+    new CoordinateMatrix(data)
+  }
+
+  def coordinateMatrixMultiply(leftMatrix: CoordinateMatrix,
+                               rightMatrix: CoordinateMatrix): CoordinateMatrix = {
+
+    val M_ = leftMatrix.entries
+      .map({ case MatrixEntry(i, j, v) => (j, (i, v)) })
+    val N_ = rightMatrix.entries
+      .map({ case MatrixEntry(j, k, w) => (j, (k, w)) })
+
+    val M_partitioned =  M_.partitionBy(new LeftMatrixPartitioner(2))
+    val N_partitioned = M_.partitionBy(new RightMatrixPartitioner(2))
+
+    val productEntries = M_partitioned
+      .join(N_partitioned)
+      .map({ case (_, ((i, v), (k, w))) => ((i, k), (v * w)) })
+      .reduceByKey(_ + _)
+      .map({ case ((i, k), sum) => MatrixEntry(i, k, sum) })
+
+    new CoordinateMatrix(productEntries)
+  }
+}
+
